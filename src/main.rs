67be0631cc3f6d3e7525e875eb9c4a34ea672fec
Paledreamer <dev@onenetbeyond.org>
2025-03-22 12:33:54 +0530

    beginnings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let server = TcpListener::bind("localhost:8080").await?;
+    loop {
+        let (mut tcp, _) = server.accept().await?;
+        let (reader, writer) = tcp.split();
+        let mut stream = FramedRead::new(reader, LinesCodec::new());
+        let mut sink = FramedWrite::new(writer, LinesCodec::new());
+    }
 }
